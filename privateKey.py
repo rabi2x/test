35e6ddcaad4b24b7aff2ea1e19d9d60453e4e34f
rabi2x <rafalbirecki@yahoo.com>
2022-10-30 23:05:30 +0100

    keys from .112

---
+++ privateKey.py
@@ -0,0 +1,8 @@
+dbname_www          = 'sqlite://///var/www/datacom_.db'
+dbname_home         = '/home/pi/atmospi/log.db'
+API_KEY             = 'Vfwp1Upp4oStbwBfuMeoZteaR'
+API_SECRET          = 'fFtyP4d1NAyDYL8FP6cbg2YtwJS3bb9ZbthpNnxs9lznToaDW6'
+ACCESS_TOKEN        = '869142424836354049-QCOQNYXBKH6FpTqKEclNs1RreLYFcYQ'
+ACCESS_TOKEN_SECRET = 'cvD6GDXVQMx3FRvjoF8sFi94rBVg8Z69mgajfKV87eWH5'
+temp_crit_down      = -15
+temp_crit_up        = 35
